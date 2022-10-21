05a9f70494f1e2dc821f1425de0a45536cd677eb
NikkoB702 <80132877+NikkoB702@users.noreply.github.com>
2022-10-21 10:10:29 -0700

    2nd upload

--- schema.sql
+++ schema.sql
@@ -60,4 +60,59 @@ CREATE TABLE titles (
 );
 
 
-SELECT * FROM departments;
+SELECT * FROM titles;
+
+
+ --Selecting people whose birthdays are between 1952/01/01-1955/12/31 for retirement eligibility (90,000+ people)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'; 
+
+
+--Refining the selection further to only look for people of retirement age born in 1952 (21,209 people)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31'; 
+
+
+--Refining the selection further to only look for people of retirement age born in 1953 (22,857 people)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31'; 
+
+
+--Refining the selection further to only look for people of retirement age born in 1954 (23,228 people)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'; 
+
+
+--Refining the selection further to only look for people of retirement age born in 1955 (23,104 people)
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'; 
+
+
+-- Retirement eligibility (41,380)
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Number of employees retiring 'count'
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+--Running the same code above and saving it into a new table with an INTO statement
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT * FROM retirement_info;
