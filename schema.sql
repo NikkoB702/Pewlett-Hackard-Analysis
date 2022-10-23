ac50a3cfc2c35a275f1e645612320a54378d41b7
NikkoB702 <80132877+NikkoB702@users.noreply.github.com>
2022-10-23 09:43:49 -0700

    formatting update

--- schema.sql
+++ schema.sql
@@ -93,7 +93,7 @@ FROM employees
 WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'; 
 
 
--- Retirement eligibility (41,380)
+-- Retirement eligibility (41,380 people)
 SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
